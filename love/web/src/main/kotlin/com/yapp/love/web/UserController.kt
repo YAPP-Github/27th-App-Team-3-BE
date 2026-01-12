9ade7b2705e5e42b0e148f3a2603f15c62f5593b
Gayeong Park <maroony55@gmail.com>
2026-01-13 02:47:34 +0900

    chore: 사용되지 않는 API 삭제

--- love/web/src/main/kotlin/com/yapp/love/web/UserController.kt
+++ love/web/src/main/kotlin/com/yapp/love/web/UserController.kt
@@ -3,18 +3,15 @@ package com.yapp.love.web
 import com.yapp.love.application.user.UserService
 import com.yapp.love.domain.user.model.User
 import com.yapp.love.web.auth.AuthUser
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "User", description = "유저 API")
 @RestController
 @RequestMapping("/api/v1/users")
 class UserController(
     private val userService: UserService,
 ) {
-    @GetMapping
-    fun getAllUsers(): List<User> {
-        return userService.getAllUsers()
-    }
-
     @GetMapping("/me")
     fun getMyInfo(
         @AuthUser userId: Long,
