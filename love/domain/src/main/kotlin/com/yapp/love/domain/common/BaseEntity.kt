b7114c4e555a8e09996dd3dd6006d0d4b424558d
dkswnkk <jjoy9810@gmail.com>
2026-01-24 14:41:47 +0900

    [issues-23] feat: BaseEntity.kt 추가

---
+++ love/domain/src/main/kotlin/com/yapp/love/domain/common/BaseEntity.kt
@@ -0,0 +1,19 @@
+package com.yapp.love.domain.common
+
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity(
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now(),
+)
