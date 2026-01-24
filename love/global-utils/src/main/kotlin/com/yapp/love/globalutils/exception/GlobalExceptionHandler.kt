9eb5deff833e9485dcf41084076c3993b54d4d04
dkswnkk <jjoy9810@gmail.com>
2026-01-24 15:32:54 +0900

    [issues-23] feat: GlobalException에서 커스텀 메시지를 출력할 수 있다.

--- love/global-utils/src/main/kotlin/com/yapp/love/globalutils/exception/GlobalExceptionHandler.kt
+++ love/global-utils/src/main/kotlin/com/yapp/love/globalutils/exception/GlobalExceptionHandler.kt
@@ -9,7 +9,6 @@ import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 import org.springframework.web.servlet.resource.NoResourceFoundException
 import java.net.BindException
 
@@ -70,19 +69,6 @@ class GlobalExceptionHandler {
             }
 
         logger.warn(e) { "Validation failed: $errorMsg" }
-
-        val error = ErrorResponse.from(globalErrorCode)
-
-        return ResponseEntity(error, globalErrorCode.getHttpStatus())
-    }
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException::class)
-    protected fun handleMethodArgumentTypeMismatchException(
-        e: MethodArgumentTypeMismatchException,
-    ): ResponseEntity<ErrorResponse> {
-        val globalErrorCode = GlobalErrorCode.INVALID_INPUT_VALUE
-
-        logger.warn(e) { "Type mismatch for parameter: ${e.name}" }
 
         val error = ErrorResponse.from(globalErrorCode)
 
