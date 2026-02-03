952bdbb68b0537795dce4fba632e1b17cbd24d44
dkswnkk <jjoy9810@gmail.com>
2026-02-03 23:08:36 +0900

    [issues-23] feat: 프로필 입력 기능 구현

---
+++ love/domain/src/main/kotlin/com/yapp/love/domain/user/UserAdditionInfoRepository.kt
@@ -0,0 +1,9 @@
+package com.yapp.love.domain.user
+
+import com.yapp.love.domain.user.model.UserAdditionInfo
+
+interface UserAdditionInfoRepository {
+    fun save(userAdditionInfo: UserAdditionInfo): UserAdditionInfo
+
+    fun findByUserId(userId: Long): UserAdditionInfo?
+}
