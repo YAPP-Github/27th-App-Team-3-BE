83ab10399e99c14354b6ae6ee1fe6ed8c84fb701
Gayeong Park <maroony55@gmail.com>
2026-02-09 23:51:05 +0900

    feat: 유저 정보 조회에 초대 코드 추가

--- love/application/src/main/kotlin/com/yapp/love/application/user/UserInfo.kt
+++ love/application/src/main/kotlin/com/yapp/love/application/user/UserInfo.kt
@@ -3,17 +3,19 @@ package com.yapp.love.application.user
 import com.yapp.love.domain.user.model.User
 import com.yapp.love.domain.user.model.UserAdditionInfo
 
-data class UserInfo (
+data class UserInfo(
     val id: Long,
     val name: String,
     val email: String,
+    val inviteCode: String?,
 ) {
     companion object {
-        fun from(user: User, userAdditionInfo: UserAdditionInfo): UserInfo {
+        fun from(user: User, userAdditionInfo: UserAdditionInfo, inviteCode: String?): UserInfo {
             return UserInfo(
                 id = user.id!!,
                 name = userAdditionInfo.nickname,
                 email = user.email,
+                inviteCode = inviteCode,
             )
         }
     }
--- love/application/src/main/kotlin/com/yapp/love/application/user/UserService.kt
+++ love/application/src/main/kotlin/com/yapp/love/application/user/UserService.kt
@@ -1,7 +1,7 @@
 package com.yapp.love.application.user
 
+import com.yapp.love.domain.onboarding.InviteCodeRepository
 import com.yapp.love.domain.user.UserAdditionInfoRepository
-import com.yapp.love.domain.user.model.User
 import com.yapp.love.domain.user.repository.UserRepository
 import com.yapp.love.globalutils.exception.GlobalErrorCode
 import com.yapp.love.globalutils.exception.GlobalException
@@ -10,12 +10,14 @@ import org.springframework.stereotype.Service
 @Service
 class UserService(
     private val userRepository: UserRepository,
-    private val userAdditionInfoRepository: UserAdditionInfoRepository
+    private val userAdditionInfoRepository: UserAdditionInfoRepository,
+    private val inviteCodeRepository: InviteCodeRepository,
 ) {
     fun getUserById(id: Long): UserInfo {
         val user = userRepository.findById(id) ?: throw GlobalException(GlobalErrorCode.NOT_FOUND, "존재하지 않는 유저입니다.")
         val userAdditionInfo = userAdditionInfoRepository.findByUserId(id) ?: throw GlobalException(GlobalErrorCode.NOT_FOUND, "존재하지 않는 유저 정보입니다.")
+        val inviteCode = inviteCodeRepository.findByCreatorId(id)?.code
 
-        return UserInfo.from(user, userAdditionInfo)
+        return UserInfo.from(user, userAdditionInfo, inviteCode)
     }
 }
